894a3aa1449c2c7d6a6a41425bc3c9921ee4ab58
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-08-04 14:53:26 +0200

    change-shape

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+// var init
+
 const shapesList = ["circle", "rectangle", "triangle"];
 let shapeId = 0;
+const toy = document.getElementById("toy");
+
+let isProcessing = false;
+let btnText = "Start!"
+
+// main functions
 
 const getShapeClass = () => {
     let result = "";
@@ -7,3 +15,47 @@ const getShapeClass = () => {
     result += prefix + shapesList[shapeId];
     return result;
 };
+
+const setShapeId = (id) => {
+    let maxId = shapesList.length - 1;
+
+    if(id > maxId)
+    {
+        id = maxId;
+    }
+    else if(id < 0)
+    {
+        id = 0;
+    }
+
+    shapeId = id;
+};
+
+const setShapeClass = () => {
+    const className = getShapeClass();
+    toy.className = className;
+};
+
+const changeToyShape = (id) => {
+    setShapeId(id);
+    setShapeClass();
+};
+
+// init div clicks
+
+const divCircle = document.getElementById("div-circle");
+const divRect = document.getElementById("div-rectangle");
+const divTri = document.getElementById("div-triangle");
+
+
+divCircle.addEventListener("click", function(){
+    changeToyShape(0);
+});
+
+divRect.addEventListener("click", function(){
+    changeToyShape(1);
+});
+
+divTri.addEventListener("click", function(){
+    changeToyShape(2);
+});
