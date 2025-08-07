47c82f58ab5ba06ea13b2450523710074b66d530
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-08-07 10:55:39 +0200

    printing-fix

--- index.js
+++ index.js
@@ -87,40 +87,64 @@ const footer = document.querySelector("footer"); // get footer
 let showing = false;
 const toggleBtn = document.createElement("button");
 toggleBtn.textContent = "Show Repo";
+toggleBtn.style.color = "white";
 control.appendChild(toggleBtn);
 
 const repoLink = "https://github.com/MrHryhorii/CSS-Advanced-3";
 let linkEl;
+// state switchers
+let isPrinting = false;
+let isDeleting = false;
 
 toggleBtn.addEventListener("click", () => {
-    if (!showing) {
-        // create a-tag
-        linkEl = document.createElement("a");
-        linkEl.href = repoLink;
-        linkEl.target = "_blank"; // new tab
-        linkEl.style.display = "inline-block";
-        linkEl.style.marginTop = "10px";
-        // add into footer
-        footer.appendChild(linkEl); 
+    if (!showing) 
+    {
+        // check process running
+        if(!isPrinting && !isDeleting)
+        {
+            linkEl = footer.querySelector("a");
+            if(!linkEl)
+            {
+                // create a-tag
+                linkEl = document.createElement("a");
+                linkEl.href = repoLink;
+                linkEl.target = "_blank"; // new tab
+                linkEl.style.display = "inline-block";
+                linkEl.style.marginTop = "10px";
+                // add into footer
+                footer.appendChild(linkEl); 
+            }        
 
-        // loop to add chars
-        let i = 0;
-        const chars = repoLink.split("");
-        const typer = setInterval(() => {
-            if (i < chars.length) {
-                linkEl.textContent += chars[i];
-                i++;
-            } else {
-                clearInterval(typer);
-            };
-        }, 50);
+            // loop to add chars
+            let i = linkEl.textContent.length; // if textContent is not empty. 0 - default
+            // start printing process
+            isPrinting = true;
+            const chars = repoLink.split("");
+            const typer = setInterval(() => {
+                if (i < chars.length) {
+                    linkEl.textContent += chars[i];
+                    i++;
+                } else {
+                    clearInterval(typer);
+                    // stop printing process
+                    isPrinting = false;
+                };
+            }, 50);
 
-        toggleBtn.textContent = "Hide Repo";
-        showing = true;
-        } else {
+            toggleBtn.textContent = "Hide Repo";
+            showing = true;
+        };
+    }
+    else 
+    {
+        // check process running
+        if(!isPrinting && !isDeleting)
+        {
             // loop to remove chars
             const chars = linkEl.textContent.split("");
             let i = chars.length;
+            // start deleting process
+            isDeleting = true;
             const deleter = setInterval(() => {
                 if (i > 0) {
                     chars.pop();
@@ -130,10 +154,13 @@ toggleBtn.addEventListener("click", () => {
                     clearInterval(deleter);
                     // remove link element
                     linkEl.remove();
+                    // stop deleting process
+                    isDeleting = false;
                 };
             }, 50);
             // change button state
             toggleBtn.textContent = "Show Repo";
             showing = false;
-        }
+        };
+    };
 });
